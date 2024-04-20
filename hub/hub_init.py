2fabbb716f1dde7d0d77f31602ebbb78293ba51a
louisholmes0217 <louisholmes0217@gmail.com>
2024-04-20 16:41:22 +0100

    Added some OS calls and pushed to access from main workstation

--- hub/hub_init.py
+++ hub/hub_init.py
@@ -5,5 +5,33 @@
 """
 import os
 
-with open("devices/test.txt", "w") as file:
-    
+# Creating directories
+def makedirs():
+    try:
+        os.mkdir("/opt/SIOT")
+        os.mkdir("/opt/SIOT/hub")
+    except PermissionError:
+        print("Insufficient permissions, cannot create directories \n'run me as root'")
+    except FileExistsError:
+        print("Files already exist")
+
+def settings_config():
+    while True:
+        print("Please enter the local IP: for the server")
+        ip = input()
+        if len(ip.split(".")) == 4:
+            for segment in ip:
+                if segment.isnumeric():
+                    continue
+                else:
+                    print("Invalid IP format, try again.")
+                    break
+        else:
+            print("Invalid IP format, try again.")
+        
+            
+settings_config()
+
+#def set_network_settings():
+#    with open("/opt/SIOT/settings.json", "w"):
+        
